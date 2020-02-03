11abbfe3daa45d40ea4449c3bdd67ffed2250548
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2020-02-02 21:33:44 -0600

    Fix memory leak

--- SmartBathroom/src/StallNumber.java
+++ SmartBathroom/src/StallNumber.java
@@ -5,19 +5,18 @@ public class StallNumber {
         this.number = number;
     }
 
-    //    Uncomment this to fix memory leak
-    //    @Override
-    //    public boolean equals(Object o) {
-    //        if (this == o) return true;
-    //        if (o == null || getClass() != o.getClass()) return false;
-    //
-    //        StallNumber that = (StallNumber) o;
-    //
-    //        return number == that.number;
-    //    }
-    //
-    //    @Override
-    //    public int hashCode() {
-    //        return number;
-    //    }
+    @Override
+    public int hashCode() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        StallNumber that = (StallNumber) o;
+
+        return number == that.number;
+    }
 }
